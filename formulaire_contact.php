75f95c36dfab599fe88ce89b4c986368e02eb5b2
JulienLemarchand <jl.julienlemarchand@gmail.com>
2018-06-20 13:49:41 +0200

    formulaire et carte

--- formulaire_contact.php
+++ formulaire_contact.php
@@ -2,25 +2,70 @@
 
 session_start();
 
-
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title></title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    </head>
-    <body>
-        <?php 
+        <title>Formulaire</title>
+        <style>
+        /* Always set the map height explicitly to define the size of the div element that contains the map. */
+    #map{
+        height: 80%;
+    }
+/* Optional: Makes the sample page fill the window. */
+    html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+    #floating-panel {
+        position: absolute;
+        top: 10px;
+        left: 25%;
+        z-index: 5;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #999;
+        text-align: center;
+        font-family: 'Roboto','sans-serif';
+        line-height: 30px;
+        padding-left: 10px;
+    }
+    </style>
+        
+        <?php
+        //header 
         include('inc/header.php');
-        require_once('inc/bdd.php');
         ?>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">
+
+<!-- une page contact avec un formulaire de contact et une google map intégrée, centrée sur la boutique avec un marqueur-->
+
+        <div class="container d-flex justify-content-center">
+
+                <div class="col-md-4">
+
                     <h2>Formulaire de contact</h2>
-                    
-                    
+
+                        <form method="post">
+                            <div class="form-group">
+                                <label>Nom</label>
+                                <input type="text" name="nom" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Prénom</label>
+                                <input type="text" name="prenom" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input type="email" name="email" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Message</label>
+                                <textarea name="message" class="form-control"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-default">Envoyer</button>
+                        </form>  
+
         <?php
         
             if(!empty($_POST)){
@@ -31,55 +76,64 @@ session_start();
                         //le paramètre nom n'existe pas ou est vide ou mauvais caracteres
                         $errors[] = 'Vous devez renseigner votre nom'; 
                     }
+                    
+
 
                     if(empty($_POST['prenom']) && strlen($_POST['prenom']) < 20 && !preg_match('#[A-z]+#', $_POST['prenom'])){
                         //le paramètre nom n'existe pas ou est vide ou mauvais caracteres
                         $errors[] = 'Vous devez renseigner votre prénom'; 
                     }
+                     
 
-                    if(!isset($_POST['email']) && strlen($_POST['email']) > 100 && !preg_match('#/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))${#', $_POST['email'])){
+
+                    if(empty($_POST['email']) && strlen($_POST['email']) > 100 && !preg_match('#/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))${#', $_POST['email'])){
                         //le paramètre email n'existe pas ou mauvaise syntaxe
                         $errors[] = 'Votre mail n\'est pas correct'; 
                     }
+                    
+                    
+                    
                     if(empty($_POST['message']) && strlen($_POST['message']) < 500){
                         //le paramètre nom n'existe pas ou est vide ou mauvais caracteres
                         $errors[] = 'Quel est votre message'; 
                     }
-                            
-                    else{//si le tableau $error n'est pas vide, on affiche les erreurs
+                         
+                    if(!empty($errors)){//si le tableau $error n'est pas vide, on affiche les erreurs
                         foreach($errors as $error){
                             echo '<p class="alert alert-danger">' . $error . '</p>';
                         }
                     }                       
             }
-            //si les champs sont vides      
-            else{
-                echo 'veuillez renseigner les champs demandés!';
-            }
-        //une page contact avec un formulaire de contact et une google map intégrée, centrée sur la boutique avec un marqueur.
-                    ?>
-                    <form method="post" action='traitement.php'>
-                        <div class="form-group">
-                            <label>Nom</label>
-                            <input type="text" name="nom" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label>Prénom</label>
-                            <input type="text" name="prenom" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label>Email</label>
-                            <input type="email" name="email" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label>Message</label>
-                            <input type="text" name="message" class="form-control">
-                        </div>
-                        <button type="submit" class="btn btn-default">Envoyer</button>
-                    </form>
-                    
-            </div>
-        </div>
         
+        ?>
+        </div>
+                    
+    
+        <div class="col-md-7">
+            
+            <h3>Notre boutique</h3>
+
+            <div id="map"></div>
+
+                <script>
+
+                function initMap() {
+                    var quiksilver = {lat: 44.838935, lng: -0.574483};
+                        var map = new google.maps.Map(document.getElementById('map'), {
+                        zoom: 15,
+                        center: quiksilver
+                        });
+                        var ici = new google.maps.Marker({
+                        position: quiksilver,
+                        map: map,
+                        });
+                    }    
+                
+                </script>       
+   
+        </div>
+            
+    
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZYD3Cuv3OtkiGITmjz_NUOVeTkUC71nw&callback=initMap"></script>
     </body>
 </html>
