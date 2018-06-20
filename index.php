c0eceb5d4526f1f727837bc0f8b79e18fc12e566
Thomas-Maisonneuve <maisonneuve.thoma@gmail.com>
2018-06-20 13:27:50 +0200

    Modif HEADER + ajout session_start

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <title>Home</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <link rel="stylsheet" href="app/css/style.css">
-</head>
-<body>
 
 <?php include('inc/header.php') ?>
 
