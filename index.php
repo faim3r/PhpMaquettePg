c193f1cfd0710a6f9582dcc816f60176d6d7599d
Thomas-Maisonneuve <maisonneuve.thoma@gmail.com>
2018-06-20 10:36:25 +0200

    Création menu
    try & catch

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+</head>
+<body>
+
+<?php include('inc/header.php') ?>
+
+</body>
+</html>
