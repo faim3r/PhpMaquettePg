8db95754d82633dc1c6cf56d7ff117708416991f
JulienLemarchand <jl.julienlemarchand@gmail.com>
2018-06-20 16:19:14 +0200

    formulaire fin

--- formulaire_contact.php
+++ formulaire_contact.php
@@ -10,27 +10,9 @@ session_start();
         <style>
         /* Always set the map height explicitly to define the size of the div element that contains the map. */
     #map{
-        height: 80%;
-    }
-/* Optional: Makes the sample page fill the window. */
-    html, body {
         height: 100%;
-        margin: 0;
-        padding: 0;
     }
-    #floating-panel {
-        position: absolute;
-        top: 10px;
-        left: 25%;
-        z-index: 5;
-        background-color: #fff;
-        padding: 5px;
-        border: 1px solid #999;
-        text-align: center;
-        font-family: 'Roboto','sans-serif';
-        line-height: 30px;
-        padding-left: 10px;
-    }
+   
     </style>
         
         <?php
@@ -39,31 +21,32 @@ session_start();
         ?>
 
 <!-- une page contact avec un formulaire de contact et une google map intégrée, centrée sur la boutique avec un marqueur-->
+    <section class="container d-flex justify-content-center mt-5 pb-5">
 
-        <div class="container d-flex justify-content-center">
+            <div class="col-md-4 text-center">
 
-                <div class="col-md-4">
-
-                    <h2>Formulaire de contact</h2>
+                    <h2 >Formulaire de contact</h2>
 
                         <form method="post">
-                            <div class="form-group">
+                            <div class="form-group mt-3">
                                 <label>Nom</label>
                                 <input type="text" name="nom" class="form-control">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group mt-3">
                                 <label>Prénom</label>
                                 <input type="text" name="prenom" class="form-control">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group mt-3">
                                 <label>Email</label>
-                                <input type="email" name="email" class="form-control">
+                                <input type="text" name="email" class="form-control">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group mt-3">
                                 <label>Message</label>
                                 <textarea name="message" class="form-control"></textarea>
                             </div>
-                            <button type="submit" class="btn btn-default">Envoyer</button>
+                            <div class="form-group mt-3">
+                            <button type="submit" class="text center btn btn-warning">Envoyer</button>
+                            </div>
                         </form>  
 
         <?php
@@ -78,19 +61,16 @@ session_start();
                     }
                     
 
-
                     if(empty($_POST['prenom']) && strlen($_POST['prenom']) < 20 && !preg_match('#[A-z]+#', $_POST['prenom'])){
                         //le paramètre nom n'existe pas ou est vide ou mauvais caracteres
                         $errors[] = 'Vous devez renseigner votre prénom'; 
                     }
                      
 
-
-                    if(empty($_POST['email']) && strlen($_POST['email']) > 100 && !preg_match('#/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))${#', $_POST['email'])){
+                    if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
                         //le paramètre email n'existe pas ou mauvaise syntaxe
                         $errors[] = 'Votre mail n\'est pas correct'; 
                     }
-                    
                     
                     
                     if(empty($_POST['message']) && strlen($_POST['message']) < 500){
@@ -100,19 +80,17 @@ session_start();
                          
                     if(!empty($errors)){//si le tableau $error n'est pas vide, on affiche les erreurs
                         foreach($errors as $error){
-                            echo '<p class="alert alert-danger">' . $error . '</p>';
+                            echo '<p class="alert alert-warning">' . $error . '</p>';
                         }
                     }                       
             }
         
         ?>
-        </div>
+            </div>
                     
     
         <div class="col-md-7">
             
-            <h3>Notre boutique</h3>
-
             <div id="map"></div>
 
                 <script>
@@ -132,8 +110,14 @@ session_start();
                 </script>       
    
         </div>
-            
-    
+    </section>
+
+    <?php
+    //header 
+    include('inc/footer.php');
+    ?>
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZYD3Cuv3OtkiGITmjz_NUOVeTkUC71nw&callback=initMap"></script>
+    
     </body>
+    
 </html>
