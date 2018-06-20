73007deeb4940628eec275f431ee96cf0fd0d4f4
blackiguane <blackiguane@users.noreply.github.com>
2018-06-20 15:21:02 +0200

    images
    img

---
+++ inc/footer.php
@@ -0,0 +1,22 @@
+<footer class="bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="m-auto">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active mr-5">
+                        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item mr-5">
+                        <a class="nav-link" href="#">Produits</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="formulaire_contact.php">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <section class="container text-center pb-3 text-light mt-2">
+        <h5>Site propulsé par Pierre, David, Julien, Stéphane et Thomas</h5>
+    </section>
+</footer>
--- inc/header.php
+++ inc/header.php
@@ -1,25 +1,34 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="index.php">Ma Boutique de OUF !!</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylsheet" href="app/css/style.css">
+</head>
+<body>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Produits</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0" method="POST">
-      <input class="form-control mr-sm-2" type="email" placeholder="E-Mail">
-      <input class="form-control mr-sm-2" type="password" placeholder="Mot de passe">
-      <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Connexion</button>
-    </form>
-  </div>
-</nav>
+<?php require_once('inc/bdd.php'); ?>
+
+<header>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.php">Ma Boutique de OUF !!</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Produits</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="formulaire_contact.php">Contact</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0" method="POST">
+        <input class="form-control mr-sm-2" type="email" placeholder="E-Mail">
+        <input class="form-control mr-sm-2" type="password" placeholder="Mot de passe">
+        <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Connexion</button>
+      </form>
+    </div>
+  </nav>
+</header>
