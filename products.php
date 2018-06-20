02ed675a8488907620a5fd6f6e1c177cc48f141d
davidba659133 <38923177+davidba659133@users.noreply.github.com>
2018-06-20 14:45:28 +0200

    requete moteur recherche
    moteur de recherche fonctionnel

--- products.php
+++ products.php
@@ -25,24 +25,100 @@ include ('inc/header.php');
         <div class="col-md-8">
             <form method="get">
                 <div class="form-group">
-                    <label>Pseudo</label>
-                    <input type="text" name="nom" class="form-control">
+                    <label>Nom produit recherché</label>
+                    <input type="text" name="nomProduit" class="form-control" value="<?php if (isset($_GET['nomProduit'])){echo $_GET['nomProduit'];}?>">
                 </div>
-                <div class="input-group">
-                    <select class="custom-select" id="inputGroupSelect04">
-                        <option selected>Choose...</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                <div class="form-group">
+                    <label>Recherche par catégorie</label>
+                    <select class="custom-select col-4" id="inputGroupSelect04" name="categories">
+                        <option selected disabled>Catégories</option>
+                        <?php
+                        //requete pour afficher les catégories
+
+                        $requete = $bdd ->query( "SELECT * from categories GROUP BY libelle");
+                        $categories = $requete ->fetchAll();
+                        foreach ($categories as $category){
+                            if ($_GET['categories'] == $categories['id']) {
+                                ?>
+                                <option selected value="<?= $category['id'] ?>"><?= $category['libelle'] ?></option>
+                                <?php
+                            }
+                            else{
+                                ?>
+                                <option value="<?= $category['id'] ?>"><?= $category['libelle'] ?></option>
+                                <?php
+                            }
+                        }
+
+                        ?>
                     </select>
-                    <div class="input-group-append">
-                        <button class="btn btn-outline-secondary" type="button">Button</button>
-                    </div>
                 </div>
-                <button type="submit" class="btn btn-info">S'inscrire</button>
+                <button type="submit" class="btn btn-info">Rechercher</button>
             </form>
         </div>
     </div>
 </div>
+
+<?php
+
+//-------------------------------------ELSE----------------------------------------------------
+
+//requête pour moteur de recherche
+
+if ($_GET) {
+    $nomProduit = strip_tags($_GET['nomProduit']);
+    if (!empty($_GET['categories'])) {
+        $verifCategories = strip_tags($_GET['categories']);
+    }
+
+    $requete = "SELECT * FROM products INNER JOIN categories ON products.categories_id = categories.id";
+
+    if (!empty($nomProduit) AND !empty($verifCategories)) {
+        $requete .= " WHERE categories.id = :categories_id AND products.name LIKE :nomProduits";
+    } elseif (!empty ($nomProduit)) {
+        $requete .= " WHERE products.name LIKE :nomProduits";
+    } elseif (!empty($verifCategories)) {
+        $requete .= " WHERE categories.id = :categories_id";
+
+    } else {
+        echo 'recherche non valide';
+    }
+
+    if ($requete) {
+        $recherche = $bdd->prepare($requete);
+        if (!empty($nomProduit) AND !empty($verifCategories)) {
+            $recherche->bindValue(':categories_id', $verifCategories);
+            $recherche->bindValue(':nomProduits', '%'.$nomProduit.'%');
+        } elseif (!empty($nomProduit)) {
+            $recherche->bindValue(':nomProduits', '%'.$nomProduit.'%');
+        } elseif (!empty($verifCategories)) {
+            $recherche->bindValue(':categories_id', $verifCategories);
+        }
+
+        $recherche->execute();
+        $articles = $recherche->fetchAll();
+
+//boucle pour afficher les résultats de la recherche
+
+        if (!empty($articles)) {
+            foreach ($articles as $article) {
+                ?>
+                <div class="show">
+                    <p><?=$article['name'].'<br>'?></p>
+                    <img src="<?=$article['photo']?>"><br></p>
+                </div>
+
+                <?php
+            }
+        }
+
+
+    }
+}
+
+
+
+
+?>
 </body>
 </html>
