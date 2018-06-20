1c24ab33498fc3eb1fa3414131eae999bee0d777
davidba659133 <38923177+davidba659133@users.noreply.github.com>
2018-06-20 16:19:40 +0200

    version final page products
    fusionnée et shirtée

--- products.php
+++ products.php
@@ -37,22 +37,20 @@ include ('inc/header.php');
                         $requete = $bdd ->query( "SELECT * from categories GROUP BY libelle");
                         $categories = $requete ->fetchAll();
                         foreach ($categories as $category){
-                            if ($_GET['categories'] == $categories['id']) {
-                                ?>
-                                <option selected value="<?= $category['id'] ?>"><?= $category['libelle'] ?></option>
-                                <?php
-                            }
-                            else{
                                 ?>
                                 <option value="<?= $category['id'] ?>"><?= $category['libelle'] ?></option>
                                 <?php
-                            }
                         }
-
                         ?>
                     </select>
+                    <div class="container">
+                        <div class="row">
+                            <button type="submit" class="btn btn-info col-2" id="submit">Rechercher</button>
+                            <a href="products.php" class="btn btn-info col-2 offset-8" id="reset">reset</a>
+                            <!--<button type="reset" class="btn btn-info col-2 offset-8" id="reset">Reset</button>-->
+                        </div>
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-info">Rechercher</button>
             </form>
         </div>
     </div>
@@ -80,7 +78,12 @@ if ($_GET) {
         $requete .= " WHERE categories.id = :categories_id";
 
     } else {
-        echo 'recherche non valide';
+
+            ?>
+
+            <h2 class="col-4 offset-4" style="color: red">Recherche non valide!</h2>
+            <?php
+
     }
 
     if ($requete) {
@@ -100,6 +103,8 @@ if ($_GET) {
 //boucle pour afficher les résultats de la recherche
 
         if (!empty($articles)) {
+            echo('<div class="container" >');
+            echo('<div class="row">');
             foreach ($articles as $article) {
                 ?>
                 <div class="show">
@@ -108,25 +113,42 @@ if ($_GET) {
                 </div>
                 <?php
             }
+            ?>
+            </div>
+            </div>
+            <?php
         }
     }
+    if (count($articles) == 0){
+        ?>
+        <h2 style="color: red" class="col-4 offset-4">Aucun produit correspondant</h2>
+        <?php
+    }
 }
+else {
+
 //----------------------- AFFICHAGE BASIQUE DES PRODUITS SOUS LE CHAMPS RECHERCHE ----------------------->
 // Requete pour affichage général des produits
-$afficheProduits = $bdd -> query('SELECT * FROM products');
-$afficheProduits = $afficheProduits->fetchAll();
-echo ('<div class="container" >');
-echo ('<div class="row">');
-foreach ($afficheProduits as $afficheProduit){?>
-    <div class="show">
-        <h3><?=$afficheProduit['name'].'<br>'?></h3>
-        <img src="<?=$afficheProduit['photo']?>"><br></p>
-    </div>
-    <?php
-}
-?>
-</div>
-</div>
+        $afficheProduits = $bdd->query('SELECT * FROM products');
+        $afficheProduits = $afficheProduits->fetchAll();
+        echo('<div class="container" >');
+        echo('<div class="row">');
+        foreach ($afficheProduits as $afficheProduit) {
+            ?>
+            <div class="show">
+                <h3><?= $afficheProduit['name'] . '<br>' ?></h3>
+                <img src="<?= $afficheProduit['photo'] ?>"><br></p>
+            </div>
+            <?php
+        }
 
+        ?>
+        </div>
+        </div>
+        <?php
+
+}
+
+?>
 </body>
 </html>
