b1bf02d24e32e1b58d703adbcd6c2fc5d7e2a11b
davidba659133 <38923177+davidba659133@users.noreply.github.com>
2018-06-21 14:26:47 +0200

    gestion de l'ajout user
    c'est fait

--- user.php
+++ user.php
@@ -32,83 +32,60 @@ if(isset($_SESSION['id']) AND ($_SESSION['role'] == 'ROLE_USER' OR $_SESSION['ro
                     <h3>Ajouter un utilisateur</h3>
                     <?php
                     require_once('inc/bdd.php');
-                    //traitement de l'inscription
-                    if(!empty($_POST)){
-                        //si le formulaire a été envoyé
-
-                        //nettoyage de post
-                        $post = [];
-
-                        foreach($_POST as $key=>$value){
-                            $post[$key] = trim(strip_tags($value));
-                        }
-
-                        $errors = [];
 
 
-                        //on doit vérfier que l'email n'est pas déjà présent dans la base
-                        $resultat = $bdd->prepare('SELECT id FROM users WHERE email = :email');
-                        $resultat->bindValue(':email', $post['email']);
-                        $resultat->execute();
-                        //on envoie toutes les réponses éventuelles dans une variable, qui sera donc un tableau (de tableau(x))
-                        $users = $resultat->fetchAll(PDO::FETCH_ASSOC);
-                        //on compte le nombre de "cases" de ce tableau
-                        if(count($users) > 0){
-                            //il y a au moins un résultat: l'email existe dans la base donc on génère une erreur
-                            $errors['email existe'] = 'l\'email est déjà présent dans la base';
-                        }
+                    ///////////////////////////////  AJOUT D'UN USER EN PHP  /////////////////////////////////
 
+                    if ($_SESSION['role'] == 'ROLE_ADMIN') {
+                        if (!empty ($_POST)) {
+                            $errors = [];
+                            $valideNom = strip_tags($_POST['nom']);
+                            $valideMdp = strip_tags($_POST['mdp']);
+                            $valideMdp2 = strip_tags($_POST['mdp2']);
+                            $valideRole = strip_tags($_POST['role']);
+                            $role = ['ROLE_ADMIN', 'ROLE_AUTEUR', 'ROLE_USER'];
 
-                        if(!isset($post['Nom']) OR !preg_match('#^[a-zA-Z]{4,10}$#', $post['Nom'])){
-                            $errors['pseudo'] = 'le pseudo doit faire entre 4 et 10 caractères';
-                        }
-
-                        if(!isset($post['mdp']) OR !isset($post['mdp2']) OR mb_strlen($post['mdp']) < 4 OR mb_strlen($post['mdp']) > 10 OR $post['mdp'] !== $post['mdp2']){
-                            $errors['mdp'] = 'le mot de passe doit faire entre 4 et 10 caractères et les deux mdp envoyés doivent être identiques';
-                        }
-
-                        if(!isset($post['email']) OR !filter_var($post['email'], FILTER_VALIDATE_EMAIL)){
-                            $errors['email'] = 'email invalide';
-                        }
-                        
-                        $roles_autorises = ['ROLE_USER', 'ROLE_AUTEUR', 'ROLE_ADMIN'];
-                        if(!in_array($post['role'], $roles_autorises)){
-                            $errors['role'] = 'role invalide';
-                        }
-
-                        if(empty($errors)){
-                            //pas d'erreur, on peut enregistrer l'utilisateur
-                            $resultat = $bdd->prepare('INSERT INTO users (Nom, email, tel, password, role) VALUES (:nom, :email, tel, :mdp, :role)');
-                            $resultat->bindValue(':nom', $post['Nom']);
-                            $resultat->bindValue(':email', $post['email']);
-                            $resultat->bindValue(':mdp', password_hash($post['mdp'], PASSWORD_DEFAULT));                            
-                            $resultat->bindValue(':role', $post['role']);
-                            if($resultat->execute()){
-                                echo '<p class="alert alert-success">inscription OK!</p>';
+                            if (empty($valideNom) OR strlen($valideNom) < 4 OR strlen($valideNom) > 10) {
+                                $errors[] = 'merci de remplir correctement votre pseudo';
                             }
-
-                        }
-                        else{
-                            foreach($errors as $error){
-                                echo '<p class="alert alert-danger">' . $error . '</p>';
+                            if (empty($_POST['email']) OR !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                                $errors[] = 'merci de donner un email valide';
                             }
+                            if (empty ($valideMdp) OR strlen($valideMdp) < 4 OR strlen($valideMdp) > 10 OR $valideMdp != $valideMdp2) {
+                                $errors[] = 'Merci de vérifier vos mots de passe';
+                            }
+                                if (empty($valideRole) OR !in_array($valideRole, $role)) {
+                                    $errors[] = 'merci de remplir correctement le champs rôle';
+                            }
+                            if (empty ($errors)) {
+                                   $ajout = $bdd->prepare('INSERT INTO users (user_name, email, password, role) VALUES(:nom, :email, :password, :role)');
+                                   $ajout->bindValue(':nom', $valideNom);
+                                   $ajout->bindValue(':email', $_POST['email']);
+                                   $ajout->bindValue(':password', password_hash($valideMdp, PASSWORD_DEFAULT));//pasword_hash permet de crypter ton mot de passe
+                                   $ajout->bindValue(':role', $_POST['role'] );
+                                   $ajout->execute();
 
+                                echo '<h2>Utilisateur ajouté</h2>';
+                            } else {
+                                foreach ($errors as $error) {
+                                    echo '<span style="color: red">'.$error . '</span><br>';
+                                }
+                            }
                         }
                     }
-
+                    else{
+                        echo '<h5 style="color: red">Tu n\'as pas le pouvoir de faire ça</h5>';
+                    }
 
                     ?>
                            <!--  /////////////////////////////////  FORMULAIRE POUR ENTRER UN UTILISATEUR /////////////////////////////////// -->
                     <form method="post">
             
                         <div class="form-group">
-                            <input type="text" name="Nom" class="form-control" placeholder="Nom">
+                            <input type="text" name="nom" class="form-control" placeholder="Nom">
                         </div>
                         <div class="form-group">
                             <input type="text" name="email" class="form-control" placeholder="Email">
-                        </div>
-                        <div class="form-group">           
-                            <input type="text" name="tel" class="form-control" placeholder="Votre télephone">
                         </div>
                         <div class="form-group">
                             <input type="password" name="mdp" class="form-control" placeholder="Votre mot de passe">
@@ -117,7 +94,7 @@ if(isset($_SESSION['id']) AND ($_SESSION['role'] == 'ROLE_USER' OR $_SESSION['ro
                             <input type="password" name="mdp2" class="form-control" placeholder="Répéter votre mot de passe">
                         </div>
                         <div class="form-group">
-                            <label>Role de l utilisateur</label>
+                            <label>Role de l'utilisateur</label>
                             <select name="role" class="form-control">
                                 <option disabled selected value="ROLE_USER">rôle</option>
                                 <?php
